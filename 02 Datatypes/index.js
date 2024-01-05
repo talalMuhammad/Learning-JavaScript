e608e5a89f629707ac11fe36bee34943acaddd22
talalMuhammmad <muhammad.talal.ehsan@gmail.com>
2024-01-06 00:53:51 +0500

    dataTypes in javaScript

---
+++ 02 Datatypes/index.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// dataTypes
+
+// alert(3 + 2) // we are using nodejs, not browser
+
+// console.log(3
+//     +
+//     3
+//     ); // output ajye ga but code readability sahi nhi he
+
+// number => 123
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "hello"); // string
+console.log(typeof 12345); // number
+console.log(typeof true); // boolean
+console.log(typeof null); // object 🤯
+console.log(typeof undefined); // undefined
