cce11fc2939e36539e7b5bc5b4a2a5b9bc5245c7
talalMuhammmad <muhammad.talal.ehsan@gmail.com>
2024-02-23 04:27:22 +0500

    javaScript
    javaScript

---
+++ javaScript Learning Topics/36 Document Object Model DOM/app.js
@@ -0,0 +1,22 @@
+/*
+
+DOM
+
+* The document Object Model (DOM) is an application programming interface (API) for manipulating HTML and XML documents. (add, remove, and modify parts of the document / HTML)
+
+* with the object model, javaScript gets all the power it needs to create dynamic HTML:
+    * javascript can change all the HTML elements in the page
+    * javascript can change all the HTML attributes in the page
+    * javaScript can change all the CSS styles in the page
+    * javaScript can remove existing HTML elements and attributes 
+    * javaScript can add new HTML elements and attributes
+    * javaScript can react to all existing HTML events in the page
+    * javaScript can create new HTML events in the Page
+
+* Note: in the DOM, all HTMLelements are defined as objects. So it will have both property and method
+
+* The document object represents your web page
+
+* if you want to access any element in an HTML page, you always start with accessing the document object
+
+*/
